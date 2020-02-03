feffe512061a1409618bef7623ba5983e21627ff
Andrew Lu <awclu@ucdavis.edu>
2020-02-02 20:20:45 -0800

    k

--- app/public/app.js
+++ app/public/app.js
@@ -164,7 +164,7 @@ function getSynergy1(synergy1){
         for (i = 1; i < response.length; i++){
         
         
-        $("new1").append('<h2 id="name1">' + response[i].name + '</h2>')
+        $("new").append('<h2 id="name1">' + response[i].name + '</h2>')
                   .append("<div><img style = 'height: 200px' src = '"+response[i].image+"' alt = img>");
         $("#name1").append('<h3><strong>Cost: </strong> <span id="cost1">' + response[i].cost + "金");
         $("#cost1").append('<h3><strong>属性1</strong> <span id="1syn1">'+ response[i].synergy1);
@@ -172,7 +172,7 @@ function getSynergy1(synergy1){
         $("#1syn2").append('<h3><strong>属性3</strong> <span id="1syn3">'+ response[i].synergy3);
         $("#1syn3").append('<h3><strong>推荐物品/笔记</strong> <span id="items">' + response[i].items);
         
-        $(".card-header").removeClass("new1");
+        $(".card-header").removeClass("new");
         }
 
 
@@ -193,7 +193,7 @@ function getSynergy2(synergy2){
 
         for (i = 1; i < response.length; i++){
         
-            $("new1").append('<h2 id="name1">' + response[i].name + '</h2>')
+            $("new").append('<h2 id="name1">' + response[i].name + '</h2>')
                       .append("<div><img style = 'height: 200px' src = '"+response[i].image+"' alt = img>");
             $("#name1").append('<h3><strong>Cost: </strong> <span id="cost1">' + response[i].cost + "金");
             $("#cost1").append('<h3><strong>属性1</strong> <span id="1syn1">'+ response[i].synergy1);
@@ -201,7 +201,7 @@ function getSynergy2(synergy2){
             $("#1syn2").append('<h3><strong>属性3</strong> <span id="1syn3">'+ response[i].synergy3);
             $("#1syn3").append('<h3><strong>推荐物品/笔记</strong> <span id="items">' + response[i].items);
             
-            $(".card-header").removeClass("new1");
+            $(".card-header").removeClass("new");
 
         }
 
@@ -222,7 +222,7 @@ function getSynergy3(synergy3){
 
         for (i = 1; i < response.length; i++){
         
-            $("new1").append('<h2 id="name1">' + response[i].name + '</h2>')
+            $("new").append('<h2 id="name1">' + response[i].name + '</h2>')
                       .append("<div><img style = 'height: 200px' src = '"+response[i].image+"' alt = img>");
             $("#name1").append('<h3><strong>Cost: </strong> <span id="cost1">' + response[i].cost + "金");
             $("#cost1").append('<h3><strong>属性1</strong> <span id="1syn1">'+ response[i].synergy1);
@@ -230,7 +230,7 @@ function getSynergy3(synergy3){
             $("#1syn2").append('<h3><strong>属性3</strong> <span id="1syn3">'+ response[i].synergy3);
             $("#1syn3").append('<h3><strong>推荐物品/笔记</strong> <span id="items">' + response[i].items);
             
-            $(".card-header").removeClass("new1");
+            $(".card-header").removeClass("new");
 
         }
 
